afe41561dc6505b45c3397b754c292b1db0b86d3
Peter Söllnbauer <peter.soellbauer@gmx.at>
2021-03-08 19:24:18 +0100

    Revert "Revert "Aufgabe 2.2 (Network) completed""
    This reverts commit b204056c552f1728cfc383f9d6fac9f29ae703fc.

--- app/src/main/java/com/example/se2einzelphase/NetworkThread.java
+++ app/src/main/java/com/example/se2einzelphase/NetworkThread.java
@@ -1,4 +1,39 @@
 package com.example.se2einzelphase;
 
-public class NetworkThread {
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class NetworkThread extends Thread{
+
+    String matNo;
+    String result;
+
+    NetworkThread(String matNo)
+    {
+        this.matNo = matNo;
+    }
+
+    public void run()
+    {
+        try {
+
+            Socket client = new Socket("se2-isys.aau.at", 53212);
+            DataOutputStream out = new DataOutputStream(client.getOutputStream());
+            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            out.writeBytes(matNo+'\n');
+
+            result = in.readLine();
+
+            client.close();
+
+        } catch (Exception e) {
+            result = "An error occured";
+        }
+    }
+
+
+
 }
